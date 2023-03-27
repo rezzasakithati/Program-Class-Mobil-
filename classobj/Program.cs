8d029b30aa0dadffe6bbd0a088b5340020a76ac5
rezzasakithati <rezza.sury@amikom.ac.id>
2023-03-27 21:37:02 +0700

    commit ke3

---
+++ classobj/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classobj
+{
+    public class Program
+        {
+            public static void Main()
+            {
+            Mobil mobil1 = new Mobil();
+            
+            mobil1.Warna = "Putih";
+            mobil1.JumlahPintu = 4;
+            mobil1.Merk = "Honda";
+            mobil1.Model = "Civic Turbo";
+            mobil1.TahunKeluar = 2021;
+            
+            mobil1.Gas(230);
+            mobil1.Klakson("tiiin");
+            mobil1.Tampilkaninfo();
+
+
+
+            Console.Write("Press <R> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.R) { }
+        }
+        }
+
+       
+
+        }
+ 
